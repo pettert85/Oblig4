13b3a6087fbab55215bbb9e20f1a6c83e6216496
Petter Thorsen <petter.thorsen85@gmail.com>
2016-09-29 20:36:50 +0200

    here we go

--- Program.java
+++ Program.java
@@ -2,7 +2,7 @@ public class Program{
 
 	public static void main (String[] args){
 
-    System.out.print("Hello World");
+    System.out.print("Hello World!");
 	 	 System.out.println(" fungerer fra sublime :) det er jo supert");
 	 	 System.out.println("Da er vi klare for prosjektet");
 
