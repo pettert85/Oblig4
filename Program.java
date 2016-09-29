cb379ca70aaf1bc43e38be4c314f23c7be9031ea
pettert85 <pettert85@gmail.com>
2016-09-29 13:26:41 +0200

    petter
    Jeg har tulla med fila

--- Program.java
+++ Program.java
@@ -3,7 +3,7 @@ public class Program{//ctrl + shift + 3
 	public static void main (String[] args){
 
     System.out.print("Hello World");
-	 	 System.out.println("mora di og fatter'n din");
+	 	 System.out.println("petter rulerfjkfbjkjdbfkjbwef di og fatter'n din");
 
   	}
 }
