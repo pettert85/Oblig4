bab2fef433ba4b9456099eab709275e281cd8288
BenniBanjo <bennibanjo80@gmail.com>
2016-09-29 13:09:48 +0200

    Fixed that SHIT!

--- Program.java
+++ Program.java
@@ -1,6 +1,9 @@
-public class program{//ctrl + shift + 3
-  public static void main (String[] args){
-    System.out.print("Hello World");
-	System.out.println("mora di og fatter'n");
+public class Program{//ctrl + shift + 3
 
-  }
+	public static void main (String[] args){
+
+    System.out.print("Hello World");
+	 	 System.out.println("mora di og fatter'n din");
+
+  	}
+}
