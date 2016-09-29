d96f1c42ef74a52b2b0933cfd2f700f785d5822d
Petter Thorsen <petter.thorsen85@gmail.com>
2016-09-29 18:56:56 +0200

    her er den

--- Program.java
+++ Program.java
@@ -1,10 +1,11 @@
-public class Program{//ctrl + shift + 3
+public class Program{
 
 	public static void main (String[] args){
 
     System.out.print("Hello World");
 	 	 System.out.println(" fungerer fra sublime :) det er jo supert");
-	 	 System.out.println("og igjen tralala");
+	 	 System.out.println("og igj");
 
   	}
 }
+
