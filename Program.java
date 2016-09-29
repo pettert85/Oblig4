20419ab8daf8ce7353d223327de90badeb7ec53c
Petter Thorsen <petter.thorsen85@gmail.com>
2016-09-29 15:00:06 +0200

    petter er best

--- Program.java
+++ Program.java
@@ -3,7 +3,7 @@ public class Program{//ctrl + shift + 3
 	public static void main (String[] args){
 
     System.out.print("Hello World");
-	 	 System.out.println("artig man har det");
+	 	 System.out.println(" fungerer fra sublime :)");
 
   	}
 }
