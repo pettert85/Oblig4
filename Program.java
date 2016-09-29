d51eead365e87ea9baab7bb0a3bf78f8b466411c
BenniBanjo <bennibanjo80@gmail.com>
2016-09-29 13:39:06 +0200

    Mohahahah!

--- Program.java
+++ Program.java
@@ -3,7 +3,7 @@ public class Program{//ctrl + shift + 3
 	public static void main (String[] args){
 
     System.out.print("Hello World");
-	 	 System.out.println("pettehar ikke jsajjsdjfjsdjfsdfn din");
+	 	 System.out.println("petter har ikke jsajjsdjfjsdjfsdfn din eller har han?");
 
   	}
 }
