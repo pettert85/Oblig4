e291dce0b24a9ca33fa2cbdd41861b16e0d17a98
Stian <Stian@158.36.182.214>
2016-09-29 12:35:52 +0200

    Halla

--- Program.java
+++ Program.java
@@ -1,8 +1,8 @@
 public class program{
   public static void main (String[] args){
     System.out.print("Hello World");
-System.out.println("mora di");
-//heisann
-  
+	System.out.println("mora di");
+	//heisann
+	System.out.print("Hello World");
   }
 ]
