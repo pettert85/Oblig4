3b3f3facced3ac633ecec669cdf3388972e05a94
Petter Thorsen <petter.thorsen85@gmail.com>
2016-09-30 13:07:30 +0200

    begynt

--- GeometricObject.java
+++ GeometricObject.java
@@ -1,6 +1,22 @@
-public class GeometricObject{
-  System.out.print("Sphere");
-  if (true){
-	  System.out.print("Det var en gang et eller annet")
-  }
+public abstract class  GeometricObject{
+
+double thickness = 0;
+private static final double IRON_WEIGHT = 7.8; //kg/dm^3
+
+void findArea() {
+
+} //findArea
+
+void findVolume(){
+} //findVolume
+
+void findCircumference(){
+} //findCircumference
+
+void findSurfacearea(){
+} //findSurfacearea
+
+void findWeight(){
+} //findWeight
+  
 }
