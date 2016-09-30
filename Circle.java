20ddbe9dd13b379c246a41b77684b82614f3dc12
Codemaster64 <naits97@gmail.com>
2016-09-30 14:37:55 +0200

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,5 +1,24 @@
 public class Circle extends GeometricObject {
 	
 	
+	private double radius;
 	
+		
+		public Circle(){
+			radius = 1;
+		}
+		public Circle(double r){
+			radius = r;
+		}
+		double getRadius(){
+			return radius;
+		}
+		public double findArea(){
+			return radius*radius*Math.PI; //asfjasklfjkldax
+		}
+		double findCircumference(){
+			return radius*2*Math.PI;
+		}
+		
+		
 }
