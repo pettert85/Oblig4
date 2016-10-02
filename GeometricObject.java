5eca773409cb366703825e0046fe0975402c6da6
Petter Thorsen <petter.thorsen85@gmail.com>
2016-10-02 19:10:14 +0200

    Fulgt boka side 518

--- GeometricObject.java
+++ GeometricObject.java
@@ -1,35 +1,37 @@
 public abstract class  GeometricObject{
 
-	//constructors
-	Rectangle(){
 
-	}
-
-	//Variables/Constants
 	private double thickness = 0;
 	private static final double IRON_WEIGHT = 7.8; //kg/dm^3
 
-	//Methods
-	abstract void  findArea();
+	//constructors
+	protected GeometricObject(){
+	}
 
-	abstract void findVolume();
 
-	abstract double findCircumference();
 
-	abstract void findSurfacearea();
+	//Abstract Methods
+	public abstract double findArea();
 
-	abstract void findWeight();
-  
-	//getter/setters
-	double getIronWeight(){
+	public abstract double findVolume();
+
+	public abstract double findCircumference();
+
+	public abstract double findSurfacearea();
+
+	public abstract double findWeight();
+	
+	
+	//methods
+	public double getIronWeight(){
 		return IRON_WEIGHT;
 	} //getIronWeight
 
-	 double getThickness(){
+	public double getThickness(){
 		return thickness;
 	} //getThickness
 
-	void setThickness(double x){
+	public void setThickness(double x){
 		thickness = x;
 	} //setThickness
 
