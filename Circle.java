647b6b710e3cd0d7405037ff0bb29c2245426250
Petter Thorsen <petter.thorsen85@gmail.com>
2016-10-02 19:54:18 +0200

    ordnet litt

--- Circle.java
+++ Circle.java
@@ -1,22 +1,41 @@
-public class Circle extends GeometricObject {
+public abstract class Circle extends GeometricObject { 
+	
+	/*må være abstrakt ellers må alle abstrakte metoder fra GeometricObjects implementeres
+	  Vi behøver vel strengt talt ikke volum og vekt av bare en sirkel :)
+
+	  en default constructor og en som setter radius + nødvendige metoder for findArea, FindCircumference
+	  og getRadius og setRadius
+																										*/ 
 	
 	private double radius;
 	
-		public Circle(){
-			radius = 1;
-		}
-		public Circle(double r){
-			radius = r;
-		}
-		double getRadius(){
-			return radius;
-		}
-		public double findArea(){
-			return radius*radius*Math.PI; //asfjasklfjkldax
-		}
-		double findCircumference(){
-			return radius*2*Math.PI;
-		}
-		
-		
-}
+	//constructors
+	public Circle(){
+		radius = 0;
+	}
+
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+
+
+	//metoder
+	public double findArea(){
+		return (radius * radius) * Math.PI;
+	}
+
+	public double findCircumference(){
+		return (2 * Math.PI) * radius;
+	}
+
+
+	public double getRadius(){
+		return radius;
+	}
+
+	public void setRadius(double radius){
+
+		this.radius = radius;
+	}
+				
+} //Circel
