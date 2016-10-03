f952aaa9ff98300916d3fbe15b6cee8a4c7278ba
Petter Thorsen <petter.thorsen85@gmail.com>
2016-10-03 21:35:54 +0200

    inneholder bare metoder for sirkel

--- Circle.java
+++ Circle.java
@@ -1,54 +1,28 @@
+//behøver bare metoder som tilhører en sirkel
+
 public abstract class Circle extends GeometricObject { 
 	
-	/*må være abstrakt ellers må alle abstrakte metoder fra GeometricObjects implementeres
-	  Vi behøver vel strengt talt ikke volum og vekt av bare en sirkel :)
-
-	  en default constructor og en som setter radius + nødvendige metoder for findArea, FindCircumference
-	  og getRadius og setRadius
-																										*/ 
-	
-	private static double radius;
-	
-	//constructors
-	public Circle() {
-		radius = 0;
-	}
-
-	public Circle(double radius) {
-		this.radius = radius;
-	}
-
-
-	//metoder
-	public static double findAreaOut() {
-		return (radius * radius) * Math.PI;
-	}
-	
-	public static double findAreaInner(double t) {
-		double NewRadius = radius-t;
-		return  (NewRadius*NewRadius)* Math.PI;
+	public Circle(){
 		
-	}
+		super();
+		}
 
+	public Circle(double r, double t){
+		super(r, t);
+		} 
+
+	public Circle(double r, double t, double h){
+		super(r, t, h);
+		} 
+
+
+	//Methods
 	public  double findCircumference() {
-		return (2 * Math.PI) * radius;
+		return (2 * Math.PI) * getRadius();
 	}
 
 	public  double findArea() {
-		return Math.PI * (radius * radius);
+		return Math.PI * ( getRadius() * getRadius() );
 	}
-
-
-	public static double getRadius() {
-		return radius;
-	}
-
-	public static void setRadius(double r) {
-
-		radius = r;
-	}
-	
-	
-	
 				
 } //Circle
