dd78b106401549fa6e33d2d20ef6f1eacee5f415
Petter Thorsen <petter.thorsen85@gmail.com>
2016-10-03 21:33:53 +0200

    ryddet opp

--- GeometricObject.java
+++ GeometricObject.java
@@ -1,13 +1,32 @@
+//har flyttet flere metoder og constructorer hit da flere subklasser kommer til og benytte seg av
+//samme metoder/ constructorer - slik at de kun behøves her.
+
+
 public abstract class  GeometricObject{
 
 
-	private static double thickness = 0;
+	private static double thickness,height,radius,width;
 	private static final double IRON_WEIGHT = 7.8; //kg/dm^3
 
 	//constructors
 	protected GeometricObject(){
+		radius = 0;
+		thickness = 0;
+		height = 0;
+		width = 0;
 	}
 
+	protected GeometricObject(double r, double t) {
+		radius = r;
+		thickness = t;
+
+	}
+
+	protected GeometricObject(double r, double t, double h) {
+		radius = r;
+		thickness = t;
+		height = h;
+	}
 
 
 	//Abstract Methods
@@ -22,7 +41,7 @@ public abstract class  GeometricObject{
 	public abstract double findWeight();
 
 	
-	//methods
+	//Getter/setters
 	public static double getIronWeight(){
 		return IRON_WEIGHT;
 	} //getIronWeight
@@ -31,8 +50,24 @@ public abstract class  GeometricObject{
 		return thickness;
 	} //getThickness
 
-	public static void setThickness(double x){
-		thickness = x;
+	public static void setThickness(double t){
+		thickness = t;
 	} //setThickness
+
+	public static double getRadius(){
+		return radius;
+	} //getRadius
+
+	public static void setRadius(double r){
+		radius = r;
+	} //setRadius
+
+	public static double getHeight(){
+		return height;
+	} //getHeight
+
+	public static void setHeight(double h){
+		height = h;
+	} //setHeight
 
 }
