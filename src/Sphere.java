8737e8947d987bdb6c7549ee2a7fcd5cba20b89a
Codemaster64 <naits97@gmail.com>
2016-10-03 13:12:57 +0200

    Update Sphere.java
    Fungere nå, men er utregningen rett?

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,16 +1,16 @@
 
 public class Sphere extends Circle {
-	private static double tykkelse; //Greier ikke å endre variabelen (Static)
+	private static double tykkelse; //Greier ikke Ã¥ endre variabelen (Static)
 	
 	//constructors
 	public Sphere(){
 		
 		super();
-		double tykkelse = 1;
+		tykkelse = 1;
 		}
 	public Sphere(double r, double t){
 		super(r);
-		double tykkelse = t;
+		tykkelse = t;
 		}
 	
 	
@@ -21,33 +21,37 @@ public class Sphere extends Circle {
 	}
 	  static double findVoulumInner(){
 		
-		return 4*findAreaInner(tykkelse); //Får ikke sendt inn en verdi av tykkelse
+		return 4*findAreaInner(tykkelse); //FÃ¥r ikke sendt inn en verdi av tykkelse
 		
 	}
 	   static double findweight(){
 		  return ( findVoulumout() - findVoulumInner() ) * getIronWeight();
 		  
 	  }
+	   public static void setTykkelse(double t) {
+
+		   tykkelse = t;
+		}
 	  
 	  
-	  //Må ha disse for at den skal kjøre
+	  //MÃ¥ ha disse for at den skal kjÃ¸re
 	  
-	@Override
+	
 	public double findArea() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	@Override
+	
 	public double findVolume() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	@Override
+	
 	public double findSurfacearea() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	@Override
+
 	public double findWeight() {
 		// TODO Auto-generated method stub
 		return 0;
