6f144af73213838fb8951accc35a2f54bfdeb07d
Petter Thorsen <petter.thorsen85@gmail.com>
2016-10-04 21:47:15 +0200

    Math.pow er fin

--- Sphere.java
+++ Sphere.java
@@ -1,71 +1,33 @@
 
 public class Sphere extends Circle {
-	private static double tykkelse; 
 	
 	//constructors
-	public Sphere(){
-		
+	protected Sphere(){
 		super();
 		}
-	public Sphere(double r, double t){
+
+	protected Sphere(double r){
+		super(r);
+		}
+
+	protected Sphere(double r, double t){
 		super(r, t);
 		}
 
+
 	//Methods
-	
-	/*
+	protected double findVolume(){
+		double innerRadius = getRadius() - getThickness();
+		return (4 / 3) * Math.PI * Math.pow( innerRadius, 3 );
+	}
 
-	disse metodene må benyttes til fordel for findVolumeout/inner og findAreaout/inner
-	da de allerede er definert i GeometricObject på denne måten vil vi slippe og oprette
-	unødvendige metoder for at filen ikke må være abstract.
-
-	*/
-	public double findVolume() { 
-		// Vi må bruke denne metoden for utvendig og innvendig volum
-		return 0;
+	protected double findSurfacearea(){
+		return 4 * Math.PI * Math.pow( getRadius(), 2 );
 	}
 	
-	public double findSurfacearea() {
-		// Vi må bruke denne metoden for utvendi/innvendig areal
-		return 0;
+	protected double findWeight(){
+		double radius = getRadius();
+		return getIronWeight() * ( (4 / 3) * Math.PI * Math.pow( radius, 3 ) - findVolume() );
 	}
 
-	   public double findWeight(){
-		  return 0;
-
-
-
-
-		  //return ( ( findVolumeout() - findVolumeInner() ) * getIronWeight() );
-		  
-	  }
-
-//Disse bør ikke brukes - har tatt vare på de pga formler
-	  
-	  /*
-	  static double findVolumeout(){ 
-		return 4*findAreaOut();
-		
-	}
-	  static double findVolumeInner(){
-		
-		return 4*findAreaInner(tykkelse); 
-		
-	}
-
-	//disse må ordnes - må benytte metoden findSurfacearea istedet.
-	
-	public static double findAreaOut() {
-		return (getRadius() * getRadius() ) * Math.PI;
-	}
-	
-	public static double findAreaInner(double t) {
-		double NewRadius = getRadius() - getThickness() ;
-		return  (NewRadius*NewRadius)* Math.PI;
-		
-	}
-
-	*/
-	  
-	 
 } //Sphere
