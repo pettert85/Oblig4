30e715b7fbfc218fa16fba94a34e580cc4c6691b
Petter Thorsen <petter.thorsen85@gmail.com>
2016-10-04 22:25:52 +0200

    asd

--- Cylinder.java
+++ Cylinder.java
@@ -1,24 +1,45 @@
-/*
-Denne arver fra Circle og definerer derfor alle abstrakte metoder i Geometricobject
-dermed behøver ikke denne være abstrakt, vi kan opprette objekter gjennom denne
-
-*/
-
 public class Cylinder extends Circle { 
 
 	private static double height;
 
 	//constructors
-	public Cylinder(){
+	protected Cylinder(){
 		super();
 		height = 0;
 	}
 
-	public Cylinder(double r, double h, double t){
+	protected Cylinder(double r, double h, double t){
 		super(r, t);
 		height = h;
 	}
 
+
+	//Methods
+	
+	//Areal av grunnflate * høyde
+	protected double findVolume(){
+		double innerradius = getRadius() - getThickness();
+		double innerheight = getHeight() - 2 * getThickness();
+
+		return Math.PI * Math.pow(innerradius, 2) * innerheight;
+	}
+
+	//areal av bunn/topp/vegg
+	protected double findSurfacearea(){
+		double radius = getRadius();
+		double height = getHeight();
+
+		return 2 * Math.PI * Math.pow(radius, 2) + 2 * Math.PI * radius * height;
+	}
+	
+
+	protected double findWeight(){
+		double radius = getRadius();
+		return getIronWeight() * ( (4 / 3) * Math.PI * Math.pow( radius, 3 ) - findVolume() );
+	}
+
+
+	//gettere/settere
 	public static double getHeight(){
 		return height;
 	} //getHeight
