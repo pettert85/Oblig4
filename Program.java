e76f3d934ebf086bcdd1f3971f83772d5e5fd6b7
Codemaster64 <naits97@gmail.com>
2016-10-11 11:58:16 +0200

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,6 +1,6 @@
 /*
 Opretter her et geometrisk objekt kalt sirkel som benytter seg av klassen Cylinder fra den kan
-vi oprette objekter -  klassen Cylinder arver også Circle sine egenskaper
+vi oprette objekter -  klassen Cylinder arver ogsÃ¥ Circle sine egenskaper
 
 */
 
@@ -11,7 +11,7 @@ public class Program{
 
 	public static void main (String[] args){
 		double radius;
-		double tykkelse,height;
+		double tykkelse,height,length, width;
 		
 				Scanner input = new Scanner(System.in);
 		int figure;
@@ -25,15 +25,7 @@ public class Program{
 					}
 				
 			}while(figure<1 || figure>5);
-				System.out.print("Select a figure: \n Sphere = 1 \n Cylinder = 2 \n Cone = 3 \n Box = 4 \n Prism = 5 \n");
-			do{ 
-				figure = input.nextInt();
 			
-				if (figure<1 || figure>5){
-					System.out.println("You can only select a defined shape");
-					}
-				
-			}while(figure<1 || figure>5);
 		
 
 		if (figure == 1 ){//Sphere
@@ -75,61 +67,75 @@ public class Program{
 			System.out.print( sylinder.toString() );
 		}
 		
-/*
-
-		//Objekt sirkel
-		GeometricObject sirkel = new Cylinder(); // denne er litt merkelig men eneste måten jeg har fått det til.
-
-
-		System.out.print("Tast inn radius: ");
-		radius = input.nextDouble();
-
-		System.out.print("Tast inn Tykkelse: ");
-		tykkelse = input.nextDouble();
-
-		System.out.print("Tast inn høyde: ");
-		height = input.nextDouble();
-
-
-
-		//setter radius / tykkelse
-		Cylinder.setRadius(radius);
-		GeometricObject.setThickness(tykkelse);
-
-		System.out.println ("\nradiusen er: " + Cylinder.getRadius() );
-		System.out.println("Tykkelsen er: " + GeometricObject.getThickness() );
-		System.out.println("Jern veier: " + GeometricObject.getIronWeight() + " kg/dm^3" );
-
-		System.out.println("\nSirkelen sin omkrets er " + sirkel.findCircumference() );
-		System.out.println("Sirkelen sitt areal er " +  sirkel.findArea() );
-
-
-		System.out.println("\nKula:");
-
-
-		//opretter objekt kule
-		GeometricObject kule = new Sphere(radius, tykkelse);
-
-		System.out.println("kulas indre volum er: " + kule.findVolume() );
-		System.out.println("kulas overflateareal er: " + kule.findSurfaceArea() );
-		System.out.println("Tyngden pa kula er: " + kule.findWeight() );
-
-
-		//opretter objekt sylinder
-
+		/*
+		if (figure == 3 ){//Cone
+			do {
+				System.out.print("Tast in radius: ");
+				radius = input.nextDouble();
 	
+				System.out.print("Tast in height: ");
+				height = input.nextDouble();
+				
+				System.out.print("Tast in Thickness: ");
+				tykkelse = input.nextDouble();
+				
+				if (radius<tykkelse || height<2*tykkelse){
+					System.out.println("Thickness can not be bigger than radius or height");  
+				}
+			}while(radius<tykkelse || height<2*tykkelse);
+			
+			GeometricObject cone = new Cone(radius, tykkelse , height);
+
+			System.out.print( cone.toString() );
+		}*/
 		
+		
+		if (figure == 4 ){//Box
+			do {
+				System.out.print("Tast in lenght: ");
+				length = input.nextDouble();
+				
+				System.out.print("Tast in Width: ");
+				width = input.nextDouble();
+	
+				System.out.print("Tast in height: ");
+				height = input.nextDouble();
+				
+				System.out.print("Tast in Thickness: ");
+				tykkelse = input.nextDouble();
+				
+				if (length<2*tykkelse || height<2*tykkelse || width<2*tykkelse ){
+					System.out.println("Thickness can not be bigger than twice the length, width or height");  
+				}
+			}while(length<2*tykkelse || height<2*tykkelse || width<2*tykkelse );
+			
+			GeometricObject box = new Box(tykkelse, length , width, height);
 
-		GeometricObject sylinder = new Cylinder(radius, tykkelse , height);
-/*
-		System.out.println("sylinderens indre volum er: " + sylinder.findVolume() );
-		System.out.println("sylinderens overflateareal er: " + sylinder.findSurfaceArea() );
-		System.out.println("Tyngden til sylinderen er: " + sylinder.findWeight() );
+			System.out.print( box.toString() );
+		}
+		
+		/*
+		if (figure == 5 ){//Prism
+			do {
+				System.out.print("Tast in radius: ");
+				lenght = input.nextDouble();
+	
+				System.out.print("Tast in height: ");
+				height = input.nextDouble();
+				
+				System.out.print("Tast in Thickness: ");
+				tykkelse = input.nextDouble();
+				
+				if (radius<tykkelse || height<2*tykkelse){
+					System.out.println("Thickness can not be bigger than radius or height");  
+				}
+			}while(radius<tykkelse || height<2*tykkelse);
+			
+			GeometricObject prism = new Prism(radius, tykkelse , height);
 
-
-		System.out.print( sylinder.toString() );
-
-*/
+			System.out.print( prism.toString() );
+		}*/
+		
 
   	}
 }
