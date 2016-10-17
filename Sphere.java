b1e3a5264486d1fe8b850ab693b3e061fa0baa97
Petter Thorsen <petter.thorsen85@gmail.com>
2016-10-17 10:35:51 +0200

    ferdig

--- Sphere.java
+++ Sphere.java
@@ -1,46 +1,51 @@
+/*
+Ikke abstrakt, definerer alle metoder i GeometricObjects noen 
+blir definert i Circle klassen
+*/
 
 public class Sphere extends Circle {
 
 	//constructors
 	protected Sphere(){
 		super();
-		}
+	}
 
 	protected Sphere(double r, double t){
 		super(r, t);
-		}
+	}
 
 
 	//Methods
+
+	//finner kulas volum
+	protected double findVolume(){
+		return (4.0 / 3.0) * Math.PI * Math.pow( getRadius(), 3 );
+	}
 
 	//finner kulas indre volum
 	protected double findInnerVolume(){
 		double innerRadius = getRadius() - getThickness();
 		return (4.0 / 3.0) * Math.PI * Math.pow( innerRadius, 3 );
 	}
-	
-	protected double findVolume(){
-	
-	
-	return (4.0 / 3.0) * Math.PI * Math.pow( getRadius(), 3 );
-	
-	}
-	
 
 	//finner kulas overflateareal
 	protected double findSurfaceArea(){
 		return 4 * Math.PI * Math.pow( getRadius(), 2 );
 	}
 
-	//finder vekten av kulas tykkelse
+	//finder vekten av volumet til tykkelsen
 	protected double findWeight(){
 		return getIronWeight() * ( findVolume() - findInnerVolume() );
 	}
 
-public String toString(){
+	public String toString(){
 
-		return super.toString() + ", " + "the surfacearea is " + findSurfaceArea() + 
-								" DM/2, and the weight is " + findWeight() + "kg.";
+		return "\nSphere:\n"
+		+ super.toString() 
+		+ "\n"
+		+ "\nThe Spheres volume is: " + String.format("%.2f", findVolume() ) + " dm^3." 
+		+ "\nThe Spheres surfacearea is: " + String.format("%.2f", findSurfaceArea() )  + " dm^2."
+		+ "\nThe Spheres weight is: " + String.format("%.2f", findWeight() ) + " kg.\n"; 
 	}
 
 } //Sphere
